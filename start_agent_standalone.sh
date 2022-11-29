53a1e51d878c9fd0a80ed8b66341417f91a05611
shu1r0 <shimatani180231n@gmail.com>
2022-11-29 11:01:40 +0900

    fix run scripts

--- start_agent_standalone.sh
+++ start_agent_standalone.sh
@@ -6,10 +6,8 @@ PORT=31000
 QUEUE_NUM_PREROUTE=1
 QUEUE_NUM_POSTROUTE=10
 
-# sudo ip6tables -t mangle -I PREROUTING -j NFQUEUE --queue-num $QUEUE_NUM_PREROUTE
-# sudo ip6tables -t mangle -I POSTROUTING -j NFQUEUE --queue-num $QUEUE_NUM_POSTROUTE
-
 sudo ip6tables -t mangle -m ipv6header --soft --header ipv6-route -I PREROUTING -j NFQUEUE --queue-num $QUEUE_NUM_PREROUTE
 sudo ip6tables -t mangle -m ipv6header --soft --header ipv6-route -I POSTROUTING -j NFQUEUE --queue-num $QUEUE_NUM_POSTROUTE
 
-sudo python3 collector_agent.py -v --ip $IP --port $PORT --nfqueue_num_pre $QUEUE_NUM_PREROUTE --nfqueue_num_post $QUEUE_NUM_POSTROUTE --log_file $1
+
+sudo python3 collector_agent.py -v --ip $IP --port $PORT --nfqueue_num_pre $QUEUE_NUM_PREROUTE --nfqueue_num_post $QUEUE_NUM_POSTROUTE --log_file $1 -s --node_id $2
