864ed3ba5842e940d38349d2e591ccd1463b8bb8
shu1r0 <shu1r0.net@gmail.com>
2023-03-28 14:36:49 +0900

    update install scripts

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ if [[ $(id -u) -ne 0 ]] ; then echo "Please run with sudo" ; exit 1 ; fi
 pip3 install -r requirements.txt
 pip3 install -e .
 cp ./collector_agent.py /usr/local/bin/srv6_nfagent
+mkdir -p /var/log/srv6_ptf/
 chmod +x /usr/local/bin/srv6_nfagent
